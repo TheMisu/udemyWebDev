3814f346cbc5a2d4138b355a2801f88c154c1bc3
Mihai Paun <email.mihu@gmail.com>
2024-05-22 12:43:45 +0200

    Solved task 25.3

--- 25.3 EJS Partials/index.js
+++ 25.3 EJS Partials/index.js
@@ -3,11 +3,28 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+
+// TODO Step 2: Make sure that static files are linked to and the CSS shows up.
+// adding the static files to the Express.JS project
+app.use(express.static("public")); 
+
+
+// TODO Step 1: Render the home page "/" index.ejs
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+// TODO Step 3: Add the routes to handle the render of the about and contact pages.
+//      Hint: Check the nav bar in the header.ejs to see the button hrefs
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
 /* Write your code here:
-Step 1: Render the home page "/" index.ejs
-Step 2: Make sure that static files are linked to and the CSS shows up.
-Step 3: Add the routes to handle the render of the about and contact pages.
-  Hint: Check the nav bar in the header.ejs to see the button hrefs
 Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
 
 app.listen(port, () => {
